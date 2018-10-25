6da0d91893542935494382b5420d960f07fd531a
Soichi Hayashi <hayashis@iu.edu>
2018-10-25 14:45:44 -0400

    added export to set TMPDIR for singularity

--- start.sh
+++ start.sh
@@ -7,12 +7,17 @@ else
     mem=32gb
 fi
 
-#generate main.pbs
+#generate main
 cat <<EOF > _main
 #!/bin/bash
 #PBS -l nodes=1:ppn=4,vmem=$mem
 #PBS -l walltime=01:00:00
 #PBS -N bl-ntwrkmts
+
+if [ -d $TMPDIR ]; then
+    echo "using TMPDIR $TMPDIR for SINGULARITY_LOCALCACHEDIR"
+    export SINGULARITY_LOCALCACHEDIR=\$TMPDIR
+fi
 
 time singularity exec -e docker://brainlife/mcr:neurodebian1604-r2017a ./compiled/main
 echo "Network Pipeline Complete"
@@ -26,3 +31,5 @@ EOF
 #submit to batch scheduler
 which sbatch && sbatch --parsable -o "slurm-%j.log" -e "slurm-%j.err" _main > jobid
 which qsub && qsub -d $PWD -V -o \$PBS_JOBID.log -e \$PBS_JOBID.err _main > jobid
+
+
