a6654fa849b7f6cc61ceab828ea7c0ba16a9641b
Lindsey Kitchell <lindseykitchell21@gmail.com>
2019-01-24 12:07:52 -0500

    Update main.m

--- main.m
+++ main.m
@@ -21,8 +21,8 @@ mkdir('cache');
 disp('Loading data...');
 
 % create the labels
-labs = fullfile(config.labs, 'parc.nii.gz');
-mask = fullfile(config.labs, 'mask.nii.gz');
+labs = fullfile(config.parc, 'parc.nii.gz');
+mask = fullfile(config.mask, 'mask.nii.gz');
 infl = config.infl;
 
 % if these exist?
