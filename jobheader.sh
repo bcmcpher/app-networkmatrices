ccd196ab2db6d6e63ba714344279f0da52947e63
bcmcpher <bcmcpher@iu.edu>
2019-04-19 13:32:27 -0400

    minimized resource requirements based on job run tests

--- jobheader.sh
+++ jobheader.sh
@@ -2,10 +2,10 @@
 #figure out amount of memory we really need by parsing config.json with jq
 if [ "$(jq .comptprof -r config.json)" == "true" ]; then
     mem=64gb
-    walltime=03:00:00
+    walltime=01:00:00
 else
-    mem=32gb
-    walltime=03:00:00
+    mem=16gb
+    walltime=00:40:00
 fi
 
 echo "#PBS -l nodes=1:ppn=4,vmem=$mem"
