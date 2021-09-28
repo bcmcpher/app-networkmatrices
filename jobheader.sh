d825db0a143856510dee4de591833e237a2102d7
bcmcpher <bcmcpher@iu.edu>
2021-09-28 15:51:08 -0400

    upped walltime

--- jobheader.sh
+++ jobheader.sh
@@ -2,10 +2,10 @@
 #figure out amount of memory we really need by parsing config.json with jq
 if [ "$(jq .comptprof -r config.json)" == "true" ]; then
     mem=64gb
-    walltime=01:00:00
+    walltime=02:00:00
 else
     mem=32gb
-    walltime=00:40:00
+    walltime=01:30:00
 fi
 
 echo "#PBS -l nodes=1:ppn=4,vmem=$mem"
